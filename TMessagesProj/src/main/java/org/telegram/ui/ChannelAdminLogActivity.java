72a711d9ef0dec20490f77a65bb4fa7bd480046d
Dmitriy Babenko <dazewell@users.noreply.github.com>
2026-09-18 11:22:50 -0700

    use FQN instead of an import in ChannelAdminLogActivity to keep footprint to one line #material-design-3-chat-header

--- TMessagesProj/src/main/java/org/telegram/ui/ChannelAdminLogActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChannelAdminLogActivity.java
@@ -177,7 +177,6 @@ import me.vkryl.core.reference.ReferenceList;
 import kotlin.Unit;
 import tw.nekomimi.nekogram.ui.BottomBuilder;
 import tw.nekomimi.nekogram.NekoConfig;
-import xyz.nextalone.nagram.NaConfig;
 import tw.nekomimi.nekogram.utils.AlertUtil;
 import tw.nekomimi.nekogram.utils.ProxyUtil;
 
@@ -1170,7 +1169,7 @@ public class ChannelAdminLogActivity extends BaseFragment implements Notificatio
         contentView.setOccupyStatusBar(!AndroidUtilities.isTablet());
         contentView.setBackgroundImage(Theme.getCachedWallpaper(), Theme.isWallpaperMotion());
         actionBar.setupGlass(glassBackgroundDrawableFactory, BlurredBackgroundProviderImpl.topPanelChatActivity(resourceProvider));
-        actionBar.doNotDrawGlassHeader = NaConfig.INSTANCE.getMaterialDesign3ChatHeader().Bool();
+        actionBar.doNotDrawGlassHeader = xyz.nextalone.nagram.NaConfig.INSTANCE.getMaterialDesign3ChatHeader().Bool();
         emptyViewContainer = new FrameLayout(context);
         emptyViewContainer.setVisibility(View.INVISIBLE);
         contentView.addView(emptyViewContainer, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT, Gravity.CENTER));
